13f14c5d00eecf0565e013f7cbeca8f529179e58
KAVYA TRIVEDI ⚡ <149308388+trivedikavya@users.noreply.github.com>
2024-12-27 19:11:06 +0530

    Update react2.jsx

--- react2.jsx
+++ react2.jsx
@@ -9,4 +9,8 @@ const [a, setA] = useState(0)
     <div>
       <h1> leo {a} </h1>
       <button onClick={()=>{setA(a+10)} }> incree </button>
-  
+      <button onClick={()=>{setA(a-10)}}> decree </button>
+    </div>
+  )
+}
+
