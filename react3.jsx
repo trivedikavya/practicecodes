4592819ea876e9201a0c0000d7bd9dc2108945bb
KAVYA TRIVEDI ⚡ <149308388+trivedikavya@users.noreply.github.com>
2025-01-12 21:33:06 +0530

    Update react3.jsx

--- react3.jsx
+++ react3.jsx
@@ -1,1 +1,22 @@
+import React from 'react'
 
+const App = () => {
+
+
+  return (
+
+
+    <div>
+      <form className=''>
+
+        <input type="text" className='m-8 text-black border-black px-5 py-6 rounded' placeholder='LAKHO GABA'/>
+        <button className=' px-3 py-7 text-xl text-white bg-red-300 rounded m-7 Border-black border-solid'>SUBMIT</button>
+      </form>
+    </div>
+
+
+
+  )
+}
+
+export default App
