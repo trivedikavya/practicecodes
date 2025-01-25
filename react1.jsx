6e81a4918af964d3908e7eff758ac02f69eab938
KAVYA TRIVEDI ⚡ <149308388+trivedikavya@users.noreply.github.com>
2025-01-25 10:39:41 +0530

    Update react1.jsx

--- react1.jsx
+++ react1.jsx
@@ -15,3 +15,36 @@ const changeA = () => {
 }
 
 export default App
+// Form handling che a 
+
+import React from 'react'
+
+const App = () => {
+
+
+  const submiit = (e) => {
+    e.preventDefault()
+    console.log("submited bhai ",e)
+    // only this so not heppen default  reload 
+  }
+
+  return (
+    
+
+    <div>
+      <form className='' onSubmit={(e) => {
+        submiit(e)
+        // e is event
+      }}>
+
+        <input type="text" className='m-8 text-black border-black px-5 py-6 rounded' placeholder='LAKHO GABA'/>
+        <button className=' px-3 py-7 text-xl text-white bg-red-300 rounded m-7 Border-black border-solid' >SUBMIT</button>
+      </form>
+    </div>
+
+
+
+  )
+}
+
+export default App
