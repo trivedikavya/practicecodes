26ef857f892e825e89eff75f3b324d69cc757467
KAVYA TRIVEDI ⚡ <149308388+trivedikavya@users.noreply.github.com>
2025-06-05 18:08:51 +0530

    Update practice.js

--- practice.js
+++ practice.js
@@ -3,3 +3,14 @@ chacha = leo; // it is word No meaning brother
 for (let i = 0; i < 10; i++) {
     console.log(i);
 } // as for loop has Meaning in js so keyword
+
+
+// var and constant
+// note they both are english wORDS means that
+// var store value can be changed 
+// const store value can not be changed
+var leo = "value of pie os "; 
+var leo = "value of pie is now ";
+const pi = 3.14;
+
+console.log(leo + " = " + pi);
