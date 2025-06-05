004c792773bc73f13b1a573c1b81246c3c6bb2b6
KAVYA TRIVEDI ⚡ <149308388+trivedikavya@users.noreply.github.com>
2025-06-05 19:23:46 +0530

    Update practice.js

--- practice.js
+++ practice.js
@@ -14,3 +14,10 @@ var leo = "value of pie is now ";
 const pi = 3.14;
 
 console.log(leo + " = " + pi);
+
+// undifed and not deffined Meaning 
+console.log(b) // not defined exist hi nahi karta asa kuch
+console.log(a) // undefiened chiz toh hai per uski koy value nahi hai 
+var a ; // per ye line to define nahi thi na Line number 3 tak so they have to Give undefine Yes but its Answer is hoisting in which 
+var b; // this auto matic pins top if you write var b = 12; then it will be hoisted to the top of the file
+b = 12;
