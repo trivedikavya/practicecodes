49d695c4c5f81a5f5f0b19f2d98be37520131669
KAVYA TRIVEDI ⚡ <149308388+trivedikavya@users.noreply.github.com>
2025-06-05 19:37:32 +0530

    Update practice.js

--- practice.js
+++ practice.js
@@ -21,3 +21,16 @@ console.log(a) // undefiened chiz toh hai per uski koy value nahi hai
 var a ; // per ye line to define nahi thi na Line number 3 tak so they have to Give undefine Yes but its Answer is hoisting in which 
 var b; // this auto matic pins top if you write var b = 12; then it will be hoisted to the top of the file
 b = 12;
+
+
+// primitive & reference
+//primitive = number , string , null , undefined , boolean 
+// reference =  [] , () , {}
+// main context = asi koy bhi value jisko copy karne per main value copy na ho uska refernce copy ho jaye use reference kehte hai , aur jiska value copy ho jaye use primitive kehte hai
+var a = 10;
+var b = a; // primitive copy do changes in b will not affect a
+b = b +2 ;
+
+var c = [1,2,3]; //array do changes in d will affect c also will become same
+var d = c;
+d.pop();
