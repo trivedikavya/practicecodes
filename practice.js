a523d68bc990f7321d018134d7ff0fbb1fdc286f
KAVYA TRIVEDI ⚡ <149308388+trivedikavya@users.noreply.github.com>
2025-06-07 16:47:12 +0530

    Update practice.js

--- practice.js
+++ practice.js
@@ -5,7 +5,7 @@ for (let i = 0; i < 10; i++) {
 } // as for loop has Meaning in js so keyword
 
 
-// var and constant
+// var and constant ✔
 // note they both are english wORDS means that
 // var store value can be changed 
 // const store value can not be changed
@@ -15,15 +15,15 @@ const pi = 3.14;
 
 console.log(leo + " = " + pi);
 
-// undifed and not deffined Meaning 
+// undifed and not deffined Meaning ✔
 console.log(b) // not defined exist hi nahi karta asa kuch
 console.log(a) // undefiened chiz toh hai per uski koy value nahi hai 
-var a ; // per ye line to define nahi thi na Line number 3 tak so they have to Give undefine Yes but its Answer is hoisting in which 
+var a ; // per ye line to define nahi thi na Line number 3 tak so they have to Give undefine Yes but its Answer is hoisting✔ in which a top per define kari de 
 var b; // this auto matic pins top if you write var b = 12; then it will be hoisted to the top of the file
 b = 12;
 
 
-// primitive & reference
+// primitive & reference✔
 //primitive = number , string , null , undefined , boolean 
 // reference =  [] , () , {}
 // main context = asi koy bhi value jisko copy karne per main value copy na ho uska refernce copy ho jaye use reference kehte hai , aur jiska value copy ho jaye use primitive kehte hai
@@ -34,3 +34,19 @@ b = b +2 ;
 var c = [1,2,3]; //array do changes in d will affect c also will become same
 var d = c;
 d.pop();
+
+// conditional statemenst ✔
+if(true) {
+// you can make it a condition like 8<0 , 9==9 , etc to make statement true or false
+}else{
+//if false so this part will run 
+}
+
+//there is one more nested -loops if else if 
+if(true){
+
+}else if(true){ // if one part will work then the loop is over
+
+}else{
+
+}
