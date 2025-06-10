7104e9bcffee34feab14cb9ba84a75c4b7412a8e
KAVYA TRIVEDI ⚡ <149308388+trivedikavya@users.noreply.github.com>
2025-06-10 22:31:45 +0530

    Update advancepractice.js

--- advancepractice.js
+++ advancepractice.js
@@ -1,1 +1,17 @@
-
+// differnce between Let , const , var 
+var a = 9; // function scope 
+// in es-5 ( var) and in es-6 ( let , const ) 
+function varl(){
+    for(var i  = 0 ; i<5; i++){
+        console.log(i);
+    }
+    console.log(i); // 5
+}
+// varl(); it works 
+function letl(){
+    for(let i  = 0 ; i<5; i++){
+        console.log(i);
+    }
+    console.log(i); // 5
+}
+letl(); // ReferenceError: i is not defined
