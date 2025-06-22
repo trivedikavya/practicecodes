3d2801031d1d43e958301477ed4c494d9c608052
KAVYA TRIVEDI ⚡ <149308388+trivedikavya@users.noreply.github.com>
2025-06-22 11:32:54 +0530

    Update endgame.js

--- practice js/endgame.js
+++ practice js/endgame.js
@@ -13,3 +13,27 @@ function snaxx(val){
 }
 // 3rd Ola foreach vada 
 // in short je functions functions ni ander functions accept kare or return kare o higher order functions
+
+
+// construre function ( a Traye use kariye jyare tamare ek jevi Property ane method ni jarur hoye)
+// je function No ek Ackkho prakar ready Kare then You can use it and fORM instance of it 
+// basic 
+function sachha(){
+    this.name = "sachha";
+    this.color = "red";
+    this.taste = "sweet";
+}
+ var bs1 = new sachha();
+ var bs2 = new sachha();
+// if Inputs ma cutamize Value levi hoye Toh 
+
+// a Adnvance use of constructor function is to create multiple objects with similar properties and methods
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+// method to display person information
+var p1 = new Person("John", 30);
+var p2 = new Person("John", 35);
+var p3 = new Person("John", 36);
+var p4 = new Person("John", 38);
