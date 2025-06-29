492641291191180fca76a57b5a490f3f1a6626ba
KAVYA TRIVEDI ⚡ <149308388+trivedikavya@users.noreply.github.com>
2025-06-29 21:32:22 +0530

    Update endgame.js

--- practice js/endgame.js
+++ practice js/endgame.js
@@ -37,3 +37,52 @@ var p1 = new Person("John", 30);
 var p2 = new Person("John", 35);
 var p3 = new Person("John", 36);
 var p4 = new Person("John", 38);
+
+// first class functions
+// theroritical concept 
+// how it is possible to pass Function in function it is Due to first class func 
+// eg :- 
+
+function abcd(){
+
+}
+abcd(function(){}) // How this is Possible 
+setInterval(function(){}) // kari rite possible It is Due to first class function
+
+
+// new keyword 
+// new keyword is a keyword which create new object for constructor function
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}   
+var p1 = new Person("John", 30);
+
+
+//iife - imediatly invoked function expression 
+// jquery , angular js , have Already used this concept
+// function ko turat chalane ki kala , in that way that we can make private variable
+// like this - gasp , $()
+// sytax make a function without name then Cover it with parenthesis and then call it at the end 
+//ye Private variable hai - sytax 
+(function(){
+
+
+})() // this is iife
+// private Function ave tyare ani ander no variable Access denia kari nakhe 
+// note Let ni value change thayi sake ( Means let age = 12; so Can age =13)
+// const can Never be changed
+// now check that Leo is private variable below
+var ans = (function(){ // given the name to the private function
+    var leo = 12;
+// we are returning an object 
+    return {
+        getter: function(){ // acess karvi Hoye toh value so ( 1. Ans ( func name) then 2. ans.getter() )
+            console.log(leo);
+        },
+        setter: function(value){ // set karvi Hoye toh value so ( 1. Ans ( func name) then 2. ans.setter(12) )
+            leo = value;
+        }
+    }
+})() // This last bractes is calling the function immediately
+// 
