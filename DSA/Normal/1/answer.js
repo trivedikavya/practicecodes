e36cacea77695cb3f7ac7da01c54d893c876dd9b
KAVYA TRIVEDI ⚡ <149308388+trivedikavya@users.noreply.github.com>
2025-11-01 22:47:53 +0530

    Implement chocolate calculation based on inputs
    Added a JavaScript program to calculate chocolates based on user input.

---
+++ DSA/Normal/1/answer.js
@@ -0,0 +1,10 @@
+let r = Number(prompt("rs bol ni value"));
+let w = Number(prompt("wrapper bol ni value"));
+let c;
+let t = r-1;
+let y = w-1;
+console.log("Chaolates" ,c = r + t/y);
+//learned 2 things in js We Can take Inputs Through Prompt("") Not only alerts 
+// We can Do type converstion In js By this 
+// ek chain create Thaye che so Recuurence Questions so The Formula is 
+//t = n + (n-1/k-1)
